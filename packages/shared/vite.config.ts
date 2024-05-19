1a255c055ecabaf4b8f1157de55c990cd95a155c
ruan-cat <1219043956@qq.com>
2024-05-19 23:01:14 +0800

    ⚙️ config: rollupOptions

--- packages/shared/vite.config.ts
+++ packages/shared/vite.config.ts
@@ -19,7 +19,23 @@ export default defineConfig({
       // 产物文件名称
       fileName: "openx-rc-shared",
     },
+
     // 为了方便学习，查看构建产物，将此置为 false，不要混淆产物代码
     minify: false,
+
+    // 参考：https://cn.vitejs.dev/config/build-options.html#build-rollupoptions
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: [/lodash.*/],
+
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量。即使不设置，构建工具也会为我们自动生成。个人倾向于不设置
+        /*
+        globals: {
+          lodash: 'lodash'
+        }
+        */
+      },
+    },
   },
 });
