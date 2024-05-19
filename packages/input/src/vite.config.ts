7a46a6820837d52c5bfd7fbc56ba68715868ea26
ruan-cat <1219043956@qq.com>
2024-05-19 23:32:54 +0800

    🤔 save-file: input 组件 ，打包莫名其妙报错；

---
+++ packages/input/src/vite.config.ts
@@ -0,0 +1,23 @@
+// packages/input/vite.config.ts
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  // 增加插件的使用
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: "./src/index.ts",
+      name: "OpenxRcUIInput",
+      fileName: "openx-rc-Input",
+    },
+    minify: false,
+    rollupOptions: {
+      external: [
+        // 除了 @openx-rc/shared，未来可能还会依赖其他内部模块，不如用正则表达式将 @openx-rc 开头的依赖项一起处理掉
+        /@openx-rc.*/,
+        "vue",
+      ],
+    },
+  },
+});
