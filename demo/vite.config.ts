12fa8481c527ac3358c614c98af2acb01e636164
ruan-cat <1219043956@qq.com>
2024-05-20 22:22:51 +0800

    ✨ feat: monorepo 即刻响应

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,7 +1,18 @@
+// demo/vite.config.ts
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { join } from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+
+  resolve: {
+    alias: [
+      {
+        find: /^@openx-rc\/(.+)$/,
+        replacement: join(__dirname, "..", "packages", "$1", "src"),
+      },
+    ],
+  },
 });
