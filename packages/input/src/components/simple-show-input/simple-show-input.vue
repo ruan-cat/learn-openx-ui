937f75c98aa82a9cc9fba71e34ffb4ae2eb7c4b8
ruan-cat <1219043956@qq.com>
2024-05-20 23:11:54 +0800

    🤔 save-file: 尝试自己新建一个局部tsconfig.json配置；

---
+++ packages/input/src/components/simple-show-input/simple-show-input.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import { ref, computed, watch } from "vue";
+
+const modelValue = defineModel({ type: String });
+</script>
+
+<template>
+  <section class="simple-show-input-root">
+    <input class="openx-input" type="text" v-model="modelValue" />
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.simple-show-input-root {
+}
+</style>
