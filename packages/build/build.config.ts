d3eee3399941862e99ae936dc25c363a1ac4f276
ruan-cat <1219043956@qq.com>
2024-07-05 11:51:57 +0800

    构建 Vue 组件模块的预设

--- packages/build/build.config.ts
+++ packages/build/build.config.ts
@@ -17,3 +17,14 @@ export function generateConfig(
     ...customOptions,
   }, viteConfig);
 }
+
+/** 构建 Vue 组件模块的预设 */
+export function generateVueConfig(
+  customOptions?: GenerateConfigOptions,
+  viteConfig?: UserConfig,
+) {
+  return generateConfig({
+    pluginVue: true,
+    ...customOptions,
+  }, viteConfig);
+}
