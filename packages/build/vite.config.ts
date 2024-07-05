014daf46a61fd1d2aab339d7c7596fa7c9e47959
ruan-cat <1219043956@qq.com>
2024-07-05 16:51:46 +0800

    ⚙️ config: 生成打包报告文档；

--- packages/build/vite.config.ts
+++ packages/build/vite.config.ts
@@ -1,4 +1,10 @@
 // packages/build/vite.config.ts
-import { generateConfig } from './build.config';
+import { generateConfig } from "./build.config";
 
-export default generateConfig();
+export default generateConfig({
+  pluginVisualizer: {
+    filename: "./dist/visualizer.html",
+    gzipSize: true,
+    template: "treemap",
+  },
+});
