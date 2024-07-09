60bac5c888b3e10a2d8f0d2b8d5bf386a4044177
ruan-cat <1219043956@qq.com>
2024-07-09 16:15:21 +0800

    🗑 del: 删除掉多余的类型声明；

--- packages/input/src/components/simple-show-input/simple-show-input.vue
+++ packages/input/src/components/simple-show-input/simple-show-input.vue
@@ -1,8 +1,4 @@
 <script lang="ts" setup>
-// import { ref, computed, watch } from "vue";
-
-// FIXME: 这里没有充足的类型声明提示
-// import SimpleShowCard from "@input/components/simple-show-card/simple-show-card.vue";
 import SimpleShowCard from "../simple-show-card/simple-show-card.vue";
 
 defineOptions({
--- packages/input/src/index.ts
+++ packages/input/src/index.ts
@@ -1,7 +1,5 @@
 // packages/input/src/index.ts
 import Input from "./input.vue";
-// import SimpleShowCard from "@input/components/simple-show-card/simple-show-card.vue";
-// import SimpleShowInput from "@input/components/simple-show-input/simple-show-input.vue";
 import SimpleShowCard from "./components/simple-show-card/simple-show-card.vue";
 import SimpleShowInput from "./components/simple-show-input/simple-show-input.vue";
 
